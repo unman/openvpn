476413dcd318ac1739f88eca40618c371da245f8
unman <unman@thirdeyesecurity.org>
2024-02-05 23:54:43 +0000

    Update script for up/down actions

--- qubes-vpn-handler.sh
+++ qubes-vpn-handler.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 export PATH="$PATH:/usr/sbin:/sbin"
+nft list chain qubes output > /rw/config/vpn/output
+sed -i '1 i #!/usr/sbin/nft -f ' output
+sed -i '1 a flush chain qubes output ' output
 
 case "$1" in
 
 up)
-iptables -D OUTPUT -o eth0 -p udp --dport 53 -j ACCEPT || true
+sed -i /53/d output
+nft -f output
+nft flush chain qubes dnat-dns
 # To override DHCP DNS, assign DNS addresses to 'vpn_dns' env variable before calling this script;
 # Format is 'X.X.X.X  Y.Y.Y.Y [...]'
 if [[ -z "$vpn_dns" ]] ; then
+    echo "Getting DNS entry from DHCP " >> /rw/config/vpn/log
     # Parses DHCP foreign_option_* vars to automatically set DNS address translation:
     for optionname in ${!foreign_option_*} ; do
         option="${!optionname}"
@@ -16,12 +22,11 @@ if [[ -z "$vpn_dns" ]] ; then
     done
 fi
 
-iptables -t nat -F PR-QBS
 if [[ -n "$vpn_dns" ]] ; then
     # Set DNS address translation in firewall:
     for addr in $vpn_dns; do
-        iptables -t nat -A PR-QBS -i vif+ -p udp --dport 53 -j DNAT --to $addr
-        iptables -t nat -A PR-QBS -i vif+ -p tcp --dport 53 -j DNAT --to $addr
+	    nft insert rule qubes dnat-dns iifname vif* tcp dport 53 dnat to $addr
+	    nft insert rule qubes dnat-dns iifname vif* udp dport 53 dnat to $addr
     done
     su - -c 'notify-send "$(hostname): LINK IS UP." --icon=network-idle' user
 else
@@ -31,6 +36,9 @@ fi
 ;;
 down)
 su - -c 'notify-send "$(hostname): LINK IS DOWN !" --icon=dialog-error' user
+unset foreign_option_*
+nft -f output.bak
+nft flush chain qubes dnat-dns
 
 # Restart the VPN automatically
 sleep 5s
